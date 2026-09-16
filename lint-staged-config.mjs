e4136df5fee51ca7db42b501b26bdc4e076acd5d
Harsh Dahiya <harshdaahiya@gmail.com>
2026-09-16 11:09:03 +0530

    chore: configure path aliases and update typecheck command

--- lint-staged-config.mjs
+++
@@ -1,4 +0,0 @@
-export default {
-    "*.{js,jsx,ts,tsx}": ["prettier --write", "eslint --fix"],
-    "*.{mjs,json,css,md}": ["prettier --write"],
-};
---
+++ lint-staged.config.mjs
@@ -0,0 +1,4 @@
+export default {
+  "*.{js,jsx,ts,tsx}": ["prettier --write", "eslint --fix"],
+  "*.{mjs,json,css,md}": ["prettier --write"],
+};
