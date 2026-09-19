28f2271a47fa32dd01de1f9d72cc1436c2e2f1b4
Harsh Dahiya <harshdaahiya@gmail.com>
2026-09-19 16:38:52 +0530

    feat: sanity schema setup for the landing and sitesettings for nav and footer

--- studio/noeveka/schemaTypes/index.ts
+++ studio/noeveka/schemaTypes/index.ts
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import { siteSettings } from './site-settings'
+import { homePage } from './home-page'
+import { aboutPage } from './about-page'
+import { testimonial } from './testimonial'
+import { service } from './service'
+
+export const schemaTypes = [siteSettings, homePage, aboutPage, testimonial, service]
+
