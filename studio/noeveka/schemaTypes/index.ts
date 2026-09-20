a1e1534e7c318b653c6bc277dbd6721b68eae6e9
Harsh Dahiya <harshdaahiya@gmail.com>
2026-09-20 20:28:03 +0530

    feat: add Sanity schemas for resources and resources page

--- studio/noeveka/schemaTypes/index.ts
+++ studio/noeveka/schemaTypes/index.ts
@@ -1,8 +1,9 @@
 import { siteSettings } from './site-settings'
 import { homePage } from './home-page'
 import { aboutPage } from './about-page'
+import { resourcesPage } from './resources-page'
 import { testimonial } from './testimonial'
 import { service } from './service'
+import { resource } from './resource'
 
-export const schemaTypes = [siteSettings, homePage, aboutPage, testimonial, service]
-
+export const schemaTypes = [siteSettings, homePage, aboutPage, resourcesPage, testimonial, service, resource]
